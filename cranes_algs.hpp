19b1c5d3bede25c9a71353a606f5409dae0072d7
Ranny Naing <70295566+RannyNaing@users.noreply.github.com>
2023-05-14 19:47:17 -0700

    Update cranes_algs.hpp, clean it a bit

--- cranes_algs.hpp
+++ cranes_algs.hpp
@@ -40,30 +40,36 @@ path crane_unloading_exhaustive(const grid& setting) {
   // TODO: implement the exhaustive search algorithm, then delete this
   // comment.
   path best(setting);
-  for(size_t len = 0; len <= max_steps; len++){
-            for(size_t bits = 0; bits <= pow(2,len)-1; bits++){
+  for(size_t len = 0; len <= max_steps; len++)
+  {
+            for(size_t bits = 0; bits <= pow(2,len)-1; bits++)
+            {
                 path candidate(setting);
-                for(size_t k = 0; k < len; ++k){
+                for(size_t k = 0; k < len; ++k)
+                {
                     int bit;
                     bit = (bits >> k) & 1;
-                    if(bit == 1){
-                        if(candidate.is_step_valid(STEP_DIRECTION_EAST)){
+                    if(bit == 1)
+                    {
+                        if(candidate.is_step_valid(STEP_DIRECTION_EAST))
+                        {
                             candidate.add_step(STEP_DIRECTION_EAST);
                         }
-                    }else {
-                        // make sure the candidate stays inside the grid and never crosses an X cell
-                        if(candidate.is_step_valid(STEP_DIRECTION_SOUTH)){
-                            // adding a down move to candidate
+                    }
+                    else
+                    {
+                        if(candidate.is_step_valid(STEP_DIRECTION_SOUTH))
+                        {
                             candidate.add_step(STEP_DIRECTION_SOUTH);
                         }
                     }
                 }
-                // if best is None or candidate harvests more gold than best
-                if(best.last_step() == STEP_DIRECTION_START || candidate.total_cranes() > best.total_cranes()){
+                if(best.last_step() == STEP_DIRECTION_START || candidate.total_cranes() > best.total_cranes())
+                {
                     best = candidate;
                 }
             }
-        }
+          }
         return best;
     }
 
@@ -80,55 +86,46 @@ path crane_unloading_dyn_prog(const grid& setting) {
 
   using cell_type = std::optional<path>;
 
-  std::vector<std::vector<cell_type>> A(setting.rows(),
+  std::vector<std::vector<cell_type> > A(setting.rows(),
                                         std::vector<cell_type>(setting.columns()));
 
   A[0][0] = path(setting);
   assert(A[0][0].has_value());
 
-  //rubric.criterion("dynamic programming - random instances", 1, [&]() std::cout << std::endl;
-  // auto small_output = crane_unloading_dyn_prog(small_random);
-  //       TEST_EQUAL("small", 4, small_output.total_cranes());
-
-
   for (coordinate r = 0; r < setting.rows(); ++r) {
     for (coordinate c = 0; c < setting.columns(); ++c) {
 
-      if (setting.get(r, c) == CELL_BUILDING) {
+      if (setting.get(r, c) == CELL_BUILDING){
         A[r][c].reset();
         continue;
-      }
+        }
 
-      //begin if (setting.get(r, c) != CELL_BUILDING case
-      //if (setting.get(r, c) != CELL_BUILDING) {
+    cell_type from_above = std::nullopt;
+    cell_type from_left = std::nullopt;
 
-        // from_above = from_left = None
-        std::optional<path> from_above;
-        std::optional<path> from_left;
-
-        // if r greater than 0 and ! None
-        if (r > 0 && A[r-1][c].has_value()) {
-
+        if (r > 0 && A[r-1][c].has_value())
+        {
            from_above = A[r-1][c].value();
 
-           if(from_above->is_step_valid(STEP_DIRECTION_SOUTH) ) {
+           if(from_above->is_step_valid(STEP_DIRECTION_SOUTH))
+           {
               from_above->add_step(STEP_DIRECTION_SOUTH);
            }
         }
-        // if c greater than 0 and ! None
-        if (c > 0 && A[r][c-1].has_value()) {
-
+        if (c > 0 && A[r][c-1].has_value())
+        {
            from_left = A[r][c-1].value();
 
-           if(from_left->is_step_valid(STEP_DIRECTION_EAST) ) {
+           if(from_left->is_step_valid(STEP_DIRECTION_EAST))
+           {
               from_left->add_step(STEP_DIRECTION_EAST);
            }
 
         }
-        // if from_above and from_left ! None
-        if(from_above.has_value() && from_left.has_value()) {
-          //if above > left
-          if(from_above->total_cranes() > from_left->total_cranes()) {
+        if(from_above.has_value() && from_left.has_value())
+         {
+          if(from_above->total_cranes() > from_left->total_cranes())
+          {
             A[r][c] = from_above;
           }
           else
@@ -136,31 +133,33 @@ path crane_unloading_dyn_prog(const grid& setting) {
             A[r][c] = from_left;
           }
         }
-        // if from_above is non-None
-        else if(from_above.has_value()) {
+        else if(from_above.has_value())
+        {
             A[r][c] = from_above;
         }
-        // if from_left is non-None
-        else if (from_left.has_value()){
+        else if (from_left.has_value())
+        {
            A[r][c] = from_left;
         }
-      //end if (setting.get(r, c) != CELL_BUILDING case
     }
   }
 
 
   cell_type* best = &(A[0][0]);
   assert(best->has_value());
-  for (coordinate r = 0; r < setting.rows(); ++r) {
-    for (coordinate c = 0; c < setting.columns(); ++c) {
-      if (A[r][c].has_value() && A[r][c]->total_cranes() > (*best)->total_cranes()) {
+  for (coordinate r = 0; r < setting.rows(); ++r)
+  {
+    for (coordinate c = 0; c < setting.columns(); ++c)
+    {
+      if (A[r][c].has_value() && A[r][c]->total_cranes() > (*best)->total_cranes())
+      {
         best = &(A[r][c]);
       }
     }
   }
 
   assert(best->has_value());
- //   std::cout << "total cranes" << (**best).total_cranes() << std::endl;
+    std::cout << "total cranes" << (**best).total_cranes() << std::endl;
 
   return **best;
 }
